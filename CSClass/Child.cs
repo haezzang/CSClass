fda1a36fd0339f482a37dd53c6fa53bf38d83434
Haejeong Cho <s2036@e-mirim.hs.kr>
2022-06-28 11:18:53 +0900

    #23 상속의 생성자

---
+++ CSClass/Child.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSClass
+{
+    public class Child : Parent
+    {
+        private string v;
+
+
+        public Child() : base(10) 
+        {
+            Console.WriteLine("Child() : base(10)");
+        }
+        public Child(string v)
+        {
+            Console.WriteLine("Child(String input) : base(input)");
+        }
+    }
+}
---
+++ CSClass/Parent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSClass
+{
+    public class Parent
+
+
+    {
+        private int v;
+
+
+        public Parent( )
+        {
+            Console.WriteLine("Parent()");
+        }
+        public Parent(int param) {
+            Console.WriteLine("Parent(int parame)");
+        }
+        public Parent(String input)
+        {
+            Console.WriteLine("Parent(String param)");
+
+
+        }
+    }
+}
